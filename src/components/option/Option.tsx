fcd6f87486d6c8da6349630ea5ee7b51ee6c8904
Jorge Petris <jorgepetris@gmail.com>
2024-07-14 00:27:28 -0600

    Refactor code for mobile view

--- src/components/option/Option.tsx
+++ src/components/option/Option.tsx
@@ -4,17 +4,17 @@ const Option = ({ title, bgImage }: { title: string; bgImage: string }) => {
   return (
     <Container bgImage={bgImage}>
       <TitleContainer>
-        <h1 style={{ margin: "0px 12px", fontFamily: "Libre Caslon Display", fontWeight: "400" }}>{title}</h1>
+        <Title>{title}</Title>
       </TitleContainer>
+      <WonderImage src={bgImage} alt="" />
     </Container>
   );
 };
 
 const Container = styled.div<{ bgImage: string }>`
   display: block;
-  width: 500px;
-  height: 500px;
-  background-image: url(${(props) => props.bgImage});
+  width: 100%;
+  height: 100%;
   border-radius: 8px;
   position: relative;
   z-index: -1;
@@ -24,11 +24,20 @@ const TitleContainer = styled.div`
   background-color: rgba(0, 0, 0, 0.5);
   padding: 4px 0px 4px 0px;
   padding-bottom: 12px;
-  position: absolute;
   bottom: 0;
   min-width: 100%;
   text-align: right;
-  transition: all 0.5s ease;
+`;
+
+const Title = styled.span`
+font-size: 2.5em;
+margin: 0px 12px;
+font-family: "Libre Caslon Display";
+`;
+
+const WonderImage = styled.img`
+  max-width: 100%;
+  max-height: 100%;
 `;
 
 export default Option;
