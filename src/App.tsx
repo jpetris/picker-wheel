069606f95c38bdece8a782d118cf861a948cbdd9
Jorge Petris <jorgepetris@gmail.com>
2024-07-14 01:16:19 -0600

    Change image paths, again

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
 import Option from "./components/option/Option";
 import Carousel from "./components/carousel/Carousel";
-
+import babylonImg from "/src/assets/babylon.jpeg";
+import rhodesImg from "/src/assets/rhodes.jpeg";
+import gizaImg from "/src/assets/giza.jpeg";
+import ephesosImg from "/src/assets/ephesos.jpg";
+import olympiaImg from "/src/assets/olympia.jpg";
+import halicarnassusImg from "/src/assets/halicarnassus.jpg";
+import alexandriaImg from "/src/assets/alexandria.jpeg";
 import "./App.css";
 
 function App() {
   const options = [
-    <Option key="Babylon" title="Babylon" bgImage={"/src/assets/babylon.jpeg"} />,
-    <Option key="Rhodes" title="Rhodes" bgImage={"/src/assets/rhodes.jpeg"} />,
-    <Option key="Gizah" title="Gizah" bgImage={"/src/assets/giza.jpeg"} />,
-    <Option key="Ephesos" title="Ephesos" bgImage={"/src/assets/ephesos.jpg"} />,
-    <Option key="Olympia" title="Olympia" bgImage={"/src/assets/olympia.jpg"} />,
-    <Option key="Halicarnassus" title="Halicarnassus" bgImage={"/src/assets/halicarnassus.jpg"} />,
-    <Option key="Alexandria" title="Alexandria" bgImage={"/src/assets/alexandria.jpeg"} />,
+    <Option key="Babylon" title="Babylon" bgImage={babylonImg} />,
+    <Option key="Rhodes" title="Rhodes" bgImage={rhodesImg} />,
+    <Option key="Gizah" title="Gizah" bgImage={gizaImg} />,
+    <Option key="Ephesos" title="Ephesos" bgImage={ephesosImg} />,
+    <Option key="Olympia" title="Olympia" bgImage={olympiaImg} />,
+    <Option key="Halicarnassus" title="Halicarnassus" bgImage={halicarnassusImg} />,
+    <Option key="Alexandria" title="Alexandria" bgImage={alexandriaImg} />,
 
   ];
 
   return (
     <>
-      <Carousel startingOptions={options} initialSpeed={70} />
+      <Carousel startingOptions={options} initialSpeed={70}/>
     </>
   );
 }
